c25441558facd470e3deda14f591d485889168e6
bushig <bushig@mail.ru>
2016-02-06 03:31:07 +0300

    Added Category, Entry and VersionHistory models

--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Categoty(models.Model):
+    categoty=models.CharField(max_length=120, unique=True)
+
+
+class Entry(models.Model):
+    category=models.ForeignKey(Categoty)
+    name=models.CharField(max_length=120)
+    description=models.TextField(max_length=1000)
+
+
+
+class VersionHistory(models.Model):
+    entry=models.ForeignKey(Entry)
+    version=models.CharField(max_length=6)
+    timestamp=models.DateTimeField(auto_now=False, auto_now_add=True)
+    changelog=models.TextField(max_length=1000)
--- assets/views.py
+++ assets/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Categoty
 
 def assets_list(request):
-    return render(request, 'assets_list.html')
+    categories=Categoty.objects.all()
+    context={'categories':categories}
+    return render(request, 'assets_list.html', context=context)
