b700f49826b621f95179d4378a2846f02b2d0a6d
bushig <bushig@mail.ru>
2016-02-21 17:25:42 +0300

    Added assetlist tests

--- assets/tests/test_views.py
+++ assets/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase, Client
 from django.contrib.auth.models import User
 from django.core.urlresolvers import resolve
 
@@ -18,3 +18,23 @@ class AssetsURLConfTest(TestCase):
     def test_assets_add_url(self):
         found=resolve('/assets/add/')
         self.assertEqual(found.func, add_entry)
+
+
+class AssetsListViewTest(TestCase):
+
+    def setUp(self):
+        user=User(username='testuser', email='test@mail.com')
+        user.set_password('qwerty1')
+        user.save()
+
+    def test_anon_user_cant_get_add_assets_page(self):
+        c=Client()
+        response=c.get('/assets/add/')
+        self.assertEqual(response.url, '/accounts/login/?next=/assets/add/', 'Should redirect to login page')
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_can_get_add_assets_page(self):
+        c=Client()
+        c.login(username='testuser', password='qwerty1')
+        response=c.get('/assets/add/')
+        self.assertEqual(response.status_code, 200, 'Should open page')
---
+++ functional_tests/tests.py
@@ -0,0 +1,10 @@
+from django.test import LiveServerTestCase, Client
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class AssetsListTest(LiveServerTestCase):
+
+    def setUp(self):
+        pass
