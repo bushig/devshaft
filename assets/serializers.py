82123df3a614a4c41e31c5996d4e035413cebf02
bushig <bushig@mail.ru>
2016-03-01 02:03:12 +0300

    Changed serializer to display last update and total_likes(reduced db queries count)

--- assets/serializers.py
+++ assets/serializers.py
@@ -7,10 +7,13 @@ class EntrySerializer(serializers.ModelSerializer):
     # id = serializers.IntegerField(read_only=True)
     # category = serializers.IntegerField(source='category')
     user = serializers.ReadOnlyField(source='user.username')
+    # category = serializers.CharField(source='category')
+    total_likes=serializers.ReadOnlyField(source='entrylikes__count')
+    last_update=serializers.ReadOnlyField(source='versionhistory__timestamp__max')
     # total_likes = serializers.ReadOnlyField()
     class Meta:
         model = Entry
-        fields = ('id', 'category', 'user', 'name', 'description', 'total_likes')
+        fields = ('id', 'category', 'user', 'name', 'description', 'total_likes', 'last_update')
 
 class EntryLikesSerializer(serializers.ModelSerializer):
     # user = serializers.ReadOnlyField(source='user.username')
