17b8ef4095e7daf4114cd2a2bf4aaf6db0caba13
bogdan <bushig@mail.ru>
2016-06-12 02:36:54 +0300

    Defined framework attributes

--- frameworks/admin.py
+++ frameworks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Platform, Framework
+
+
+admin.site.register(Platform)
+admin.site.register(Framework)
--- frameworks/models.py
+++ frameworks/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from common.models import License
+from languages.models import Language
+
+PLATFORMS_CHOISES = ((1, 'Windows'),
+                     (2, 'Linux'),
+                     (3, 'Mac'),
+                     (4, 'Android'),
+                     (5, 'iOS'),
+                     (6, 'Web'))
+
+ORIENTATION_CHOICES = ((1, '2D&3D'),
+                       (2, '2D'),
+                       (3, '3D'))
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 
 class Framework(models.Model):
@@ -9,10 +28,12 @@ class Framework(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField(max_length=1000)
     license = models.ForeignKey(License, blank=True, null=True)
-    open_sourced = models.BooleanField()
-    #languages = many2many
-    #images
-    #likes
+    orientation = models.IntegerField(choices=ORIENTATION_CHOICES, default=1)
+    languages = models.ManyToManyField(Language)
+    target_platforms = models.ManyToManyField(Platform, related_name='target_platforms')
+    editor_platforms = models.ManyToManyField(Platform, related_name='editor_platforms')
+    # images
+    # likes
 
     class Meta:
         verbose_name_plural = 'frameworks'
