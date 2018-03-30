6c1d8de1b7a729ec80f3127be6df13510b99e313
bushig <azerot966@gmail.com>
2018-03-31 00:07:38 +0300

    Added ordering to assets

--- assets/filters.py
+++ assets/filters.py
@@ -11,20 +11,12 @@ class EntryFilter(django_filters.FilterSet):
     Filtering for Entries
     '''
     q=django_filters.CharFilter(method='filter_search', label='Search', help_text='You can search by asset name, description or asset creator')
-    # o = django_filters.OrderingFilter(fields=[] )
+    o = django_filters.OrderingFilter(fields=[('users_liked__count', 'likes'),
+                                              ('versionhistory__timestamp__max', 'version')] )
     class Meta:
         model = Entry
         # form = SearchForm
         fields = ('q', 'category') #TODO: Rename category to c/ FIX ordering
-        order_by = (('version', 'Last updated'),
-                    ('likes','Most liked'))
-
-    def get_order_by(self, order_choice):
-        if order_choice=='likes':
-            return ['-users_liked__count']
-        elif order_choice=='version':
-            return ['-versionhistory__timestamp__max'] #by most recent version
-        return super(EntryFilter, self).get_order_by(order_choice)
 
     def filter_search(self, queryset, name, value):
         return queryset.filter(Q(name__icontains=value) | Q(description__icontains=value) | Q(user__username=value))
--- assets/tests/test_views.py
+++ assets/tests/test_views.py
@@ -1,22 +1,22 @@
 from django.test import TestCase, LiveServerTestCase, Client
 from django.contrib.auth.models import User
-from django.core.urlresolvers import resolve
+from django.urls import resolve, reverse
 
-from ..views import list, entry_details, add_entry
+from ..views import assets_list, entry_details, add_entry
 
 
 class AssetsURLConfTest(TestCase):
 
     def test_assets_list_url(self):
-        found=resolve('/assets/')
-        self.assertEqual(found.func, list)
+        found=resolve(reverse('assets:list'))
+        self.assertEqual(found.func, assets_list)
 
     def test_assets_detail_url(self):
-        found=resolve('/assets/167/')
+        found=resolve(reverse('assets:detail'))
         self.assertEqual(found.func, entry_details)
 
     def test_assets_add_url(self):
-        found=resolve('/assets/add/')
+        found=resolve(reverse('assets:add_entry'))
         self.assertEqual(found.func, add_entry)
 
 
@@ -29,12 +29,12 @@ class AssetsListViewTest(TestCase):
 
     def test_anon_user_cant_get_add_assets_page(self):
         c=Client()
-        response=c.get('/assets/add/')
-        self.assertEqual(response.url, '/accounts/login/?next=/assets/add/', 'Should redirect to login page')
+        response=c.get(reverse('assets:add_entry'))
+        self.assertEqual(response.url, '/accounts/login/?next=/add/', 'Should redirect to login page')
         self.assertEqual(response.status_code, 302)
 
     def test_user_can_get_add_assets_page(self):
         c=Client()
         c.login(username='testuser', password='qwerty1')
-        response=c.get('/assets/add/')
+        response=c.get(reverse('assets:add_entry'))
         self.assertEqual(response.status_code, 200, 'Should open page')
