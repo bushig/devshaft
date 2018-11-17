763885c490a3f7302e00f7043dfe264da2af9c04
bushig <azerot966@gmail.com>
2018-11-18 02:38:36 +0300

    Main page live stats

--- apps/main_page/views.py
+++ apps/main_page/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
+from apps.assets.models import Asset
+from apps.frameworks.models import Framework
+from apps.tutorials.models import Series, Tutorial
+from zinnia.models.entry import Entry
+
+
 def main_page(request):
-    return render(request, 'main_page.html', {})
+    news_count = Entry.objects.count()
+    assets_count = Asset.objects.count()
+    frameworks_count = Framework.objects.count()
+    series_count = Series.objects.count()
+    tutorials_count = Tutorial.objects.count()
+    return render(request, 'main_page.html', {'news_count': news_count, 'assets_count': assets_count,
+                                              'frameworks_count': frameworks_count, 'series_count': series_count,
+                                              'tutorials_count': tutorials_count
+                                              })
